a8621fefdf928adf5c7ef118075ae33dc66e94c9
Irakli Sokhadze <irakli.sokhadze@outlook.com>
2025-08-31 21:52:28 +0400

    Initial Commit

---
+++ InsCounter/InsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsCounterApp.swift
+//  InsCounter
+//
+//  Created by Irakli Sokhadze on 31.08.25.
+//
+
+import SwiftUI
+
+@main
+struct InsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InsCounterTests/InsCounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InsCounterTests.swift
+//  InsCounterTests
+//
+//  Created by Irakli Sokhadze on 31.08.25.
+//
+
+import Testing
+@testable import InsCounter
+
+struct InsCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
